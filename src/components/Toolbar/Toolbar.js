4e3f33990f53e763e69a26318b00ca2a68f3c60f
aneta g <aneta.gierak@netinteractive.pl>
2019-09-16 12:35:42 +0200

    add  icons & functionality: underline, italic.

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { FaBold } from '@fortawesome/free-solid-svg-icons';
-import { faBold } from '@fortawesome/free-solid-svg-icons';
+import { faBold, faUnderline, faItalic, faAlignRight } from '@fortawesome/free-solid-svg-icons';
+
 import "./Toolbar.css";
 import { RichUtils } from "draft-js";
 
 class Toolbar extends Component {
   
-  changeTextStyle = () => {
-    this.props.handleChange(RichUtils.toggleInlineStyle(this.props.editState, "BOLD"));
+  // changeTextStyle = () => {
+  //   this.props.handleChange(RichUtils.toggleInlineStyle(this.props.editState, "BOLD"));
+  // }
+
+  changeBoldStyle = () => {
+  this.props.handleChange(RichUtils.toggleInlineStyle(this.props.editState, "BOLD"));
   }
-  isActive = () => {
+
+  changeItalicStyle = () => {
+    this.props.handleChange(RichUtils.toggleInlineStyle(this.props.editState, "ITALIC"));
+  }
+
+  changeUnderlineStyle = () => {
+    this.props.handleChange(RichUtils.toggleInlineStyle(this.props.editState, "UNDERLINE"));
+  }
+
+  isActiveBold = () => {
     const currentStyle = this.props.editState.getCurrentInlineStyle();
     return currentStyle.has("BOLD") ? "toolbar__icon active" : "toolbar__icon"
+  }
+
+  isActiveItalic = () => {
+    const currentStyle = this.props.editState.getCurrentInlineStyle();
+    return currentStyle.has("ITALIC") ? "toolbar__icon active" : "toolbar__icon"
+  }
+
+  isActiveUnderline = () => {
+    const currentStyle = this.props.editState.getCurrentInlineStyle();
+    return currentStyle.has("UNDERLINE") ? "toolbar__icon active" : "toolbar__icon"
+  }
+
+  isActiveAlignRight = () => {
+    const currentStyle = this.props.editState.getCurrentInlineStyle();
+    return currentStyle.has("ALIGNRIGHT") ? "toolbar__icon active" : "toolbar__icon"
   }
 
   render() {
     return (
       <div className="toolbar__container">
-        <button className={this.isActive()} onClick={this.changeTextStyle}>
+        <button className={this.isActiveBold()} onClick={this.changeBoldStyle}>
           <FontAwesomeIcon icon={faBold}/>
+        </button>
+        <button className={this.isActiveItalic()} onClick={this.changeItalicStyle}>
+          <FontAwesomeIcon icon={faItalic}/>
+        </button>
+        <button className={this.isActiveUnderline()} onClick={this.changeUnderlineStyle}>
+          <FontAwesomeIcon icon={faUnderline}/>
+        </button>
+        <button className={this.isActiveAlignRight()} onClick={this.changeAlignRightStyle}>
+          <FontAwesomeIcon icon={faAlignRight}/>
         </button>
       </div>
     )
